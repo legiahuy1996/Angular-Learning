6e719107e8e3e0d79e9513043a6d9790cd516568
legiahuy <legiahuy1996@outlook.com>
2019-06-10 17:38:05 +0700

    Handling Input with Two-way Binding

--- src/app/item.component.ts
+++ src/app/item.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class ItemComponent {
+    itemName: string = "Huy";
     showImage: boolean = false;
     imgWidth: number = 50;
     imgMargin: number = 2;
