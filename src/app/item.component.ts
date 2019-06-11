7bcaac99c5d51f0e61fd763d7c88363ff1560037
legiahuy <legiahuy1996@outlook.com>
2019-06-11 08:57:19 +0700

    Transforming Data with Pipes

--- src/app/item.component.ts
+++ src/app/item.component.ts
@@ -14,12 +14,14 @@ export class ItemComponent {
         {
             "ID": 1,
             "value": "one",
-            "imageSrc": "https://ttol.vietnamnetjsc.vn/images/2018/05/25/13/40/net-cuoi-be-gai-9-1527053440039156820618.jpg"
+            "imageSrc": "https://ttol.vietnamnetjsc.vn/images/2018/05/25/13/40/net-cuoi-be-gai-9-1527053440039156820618.jpg",
+            "Amount": 1000000000
         },
         {
             "ID": 2,
             "value": "two",
-            "imageSrc": "https://ttol.vietnamnetjsc.vn/images/2018/05/25/13/40/net-cuoi-be-gai-8-1527053440037953986965.jpg"
+            "imageSrc": "https://ttol.vietnamnetjsc.vn/images/2018/05/25/13/40/net-cuoi-be-gai-8-1527053440037953986965.jpg",
+            "Amount": 2000000000
         }
     ];
     toggleButton(): void {
